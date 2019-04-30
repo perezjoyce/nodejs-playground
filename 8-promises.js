c57c3227981de2ef63f6a2cb3214d8c05893df98
unknown <japerez.ph@gmail.com>
2019-04-30 17:49:32 +0800

    async and await

--- 8-promises.js
+++ 8-promises.js
@@ -17,17 +17,53 @@
 
 //====================================
 // PROMISE: an object with a few methods we can access
-const doWorkCallback = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        // resolve([1, 4, 7])
-        reject('Things went wrong!')
-    }, 2000);
+// const doWorkCallback = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         // resolve([1, 4, 7])
+//         reject('Things went wrong!')
+//     }, 2000);
+// })
+
+// //when things go well/when resolve is called
+// doWorkCallback.then((result) => {
+//     console.log('Success', result)
+//     //when reject is called
+// }).catch((error) => {
+//     console.log('Error', error)
+// })
+
+
+//====================================
+// PROMISE 
+
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
+//NESTED PROMISES
+add(1, 2).then((sum) => {
+    console.log(sum)
+
+    add(sum, 3).then((sum2) => {
+        console.log(sum2)
+    }).catch((e) => {
+        console.log(e)
+    })
+
+}).catch((e) => {
+    console.log(e)
 })
 
-//when things go well/when resolve is called
-doWorkCallback.then((result) => {
-    console.log('Success', result)
-    //when reject is called
-}).catch((error) => {
-    console.log('Error', error)
+// PROMISE CHAINING
+add(1,2).then((sum) => {
+    console.log(sum)
+    return add(sum, 4)
+}).then((sum2) => {
+    console.log(sum2)
+}).catch((e) => {
+    console.log(e)
 })
